3877acb501fd3978e6a0cc809418c5c557c48dc9
HelenaMadrid <helena.madrid@directrips.com>
2019-04-08 16:01:31 -0500

    hola

--- Moment.js
+++ Moment.js
@@ -1,127 +1,78 @@
 $(document).ready(function () {
-var config = {
-    apiKey: "AIzaSyCgZNabfQp6-0e6Q8NnB5iJcTCAX_IvOXY",
-    authDomain: "train-scheduler-7e867.firebaseapp.com",
-    databaseURL: "https://train-scheduler-7e867.firebaseio.com",
-    projectId: "train-scheduler-7e867",
-    storageBucket: "",
-    messagingSenderId: "138799081197"
-};
-firebase.initializeApp(config);
+    var config = {
+        apiKey: "AIzaSyCgZNabfQp6-0e6Q8NnB5iJcTCAX_IvOXY",
+        authDomain: "train-scheduler-7e867.firebaseapp.com",
+        databaseURL: "https://train-scheduler-7e867.firebaseio.com",
+        projectId: "train-scheduler-7e867",
+        storageBucket: "",
+        messagingSenderId: "138799081197"
+    };
+    firebase.initializeApp(config);
 
-var database = firebase.database();
+    var database = firebase.database();
 
-var train = "";
-var destination = "";
-var frequency = "";
-var nextArrival = "";
-var minutesAway = "";
-var currentTime = moment().format("HH:mm");
+    var train = "";
+    var destination = "";
+    var frequency = "";
 
-$("#first-train-time-input").timepicker({ timeFormat: "HH:mm" });
-    $("#setTimeButton").off();
-    $("#setTimeButton").on("click", function (event) {
+    $("#first-train-time-input").timepicker({ timeFormat: "HH:mm" });
+
+    $("#add-train").off();
+    $("#add-train").on("click", function (event) {
         event.preventDefault();
-        $("#first-train-time-input").timepicker("setTime", currentTime);
-       // console.log("hola "+currentTime);
+        train = $("#train-input").val().trim();
+        destination = $("#destination-input").val().trim();
+        firstTrain = $("#first-train-time-input").val().trim();
+        frequency = $("#frequency-input").val().trim();
+
+        database.ref().push({
+            TrainName: train,
+            Destination: destination,
+            Frequency: frequency,
+            FirstTrain: firstTrain
+        });
     });
+    function updateTime() {
+        var currentTime = moment().format('HH:mm:ss');
+        $("#currentTime").html("<strong>Current Time: </strong><br>"+currentTime);
+        setInterval(updateTime, 1000);
+    }
+    updateTime();
 
-$("#add-train").off();
-$("#add-train").on("click", function (event) {
-    event.preventDefault();
-    train = $("#train-input").val().trim();
-    destination = $("#destination-input").val().trim();
-    firstTrain = $("#first-train-time-input").val().trim();
-    frequency = $("#frequency-input").val().trim();
-
-    //$(".test2").text(currentTime);
-    //console.log(firstTrain);
-
-    database.ref().push({
-        TrainName: train,
-        Destination: destination,
-        Frequency: frequency,
-        FirstTrain: firstTrain,
-        //NextArrival: nextArrival,
-        //MinutesAway: minutesAway
-    });
-});
-
-database.ref().on("child_added", function (childSnapshot) {
-    var tr = $("<tr>");
-    var tdTrain = $("<td>");
-    var tdDestination = $("<td>");
-    var tdFrequency = $("<td>");
-    var tdNextArrival = $("<td>");
-    var tdMinutesAway = $("<td>");
-    var nextArrival;
-    var minutesAway;
-    currentTime = moment().format("HH:mm");
-    
-    var frequencyMin=childSnapshot.val().Frequency;
-
-    var firstTrainTime=moment(childSnapshot.val().FirstTrain, "HH:mm").subtract(1, "years");
-    var diffTime = moment().diff(moment(firstTrainTime), "minutes");
-    var tRemainder = diffTime % frequencyMin;
-    var tMinutesTillTrain = frequencyMin - tRemainder;
-    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-
-    //console.log("first train "+firstTrainTime);
-   // console.log("hour "+ hour);
-   // console.log("minutes "+ minutes);
+    database.ref().on("child_added", function (childSnapshot) {
+        var tr = $("<tr>");
+        var tdTrain = $("<td>");
+        var tdDestination = $("<td>");
+        var tdFrequency = $("<td>");
+        var tdNextArrival = $("<td>");
+        var tdMinutesAway = $("<td>");
+        var nowTime = moment();
 
 
-   // var hoursIntoMinutes=hour*60;
-    //var totalMinutes=hoursIntoMinutes+minutes;
+        var frequencyMin = childSnapshot.val().Frequency;
+        var firstTrainTime = moment(childSnapshot.val().FirstTrain, "HH:mm").subtract(1, "years");
+        var timeDifference = nowTime.diff(moment(firstTrainTime), "minutes");
+        var tRemainder = timeDifference % frequencyMin;
+        var minutesAway = frequencyMin - tRemainder;
+        var nextTrain = moment().add(minutesAway, "minutes");
 
-    //console.log("hours into minutes: "+hoursIntoMinutes);
-    //console.log("total minutes: "+totalMinutes);
+        tdTrain.attr("class", "train-name");
+        tdDestination.attr("class", "destination");
+        tdFrequency.attr("class", "frequency");
+        tdNextArrival.attr("class", "next-arrival");
+        tdMinutesAway.attr("class", "minutes-away");
 
-    console.log("first train time: "+firstTrainTime);
+        $("tbody").append(tr);
+        tr.append(tdTrain);
+        tr.append(tdDestination);
+        tr.append(tdFrequency);
+        tr.append(tdNextArrival);
+        tr.append(tdMinutesAway);
 
-    console.log("current time: "+currentTime);
-
-    console.log("frequency: "+frequencyMin);
-
-    var otherTrain=moment().add(frequencyMin, "minutes").format("HH:mm");
-    console.log("current time plus frequency which is wrong btw: "+otherTrain);
-    
-
-    tdTrain.attr("class", "train-name");
-    tdDestination.attr("class", "destination");
-    tdFrequency.attr("class", "frequency");
-    tdNextArrival.attr("class", "next-arrival");
-    tdMinutesAway.attr("class", "minutes-away");
-
-
-
-    console.log(childSnapshot.val().TrainName);
-    console.log(childSnapshot.val().Destination);
-    console.log(childSnapshot.val().Frequency);
-    //console.log(childSnapshot.val().NextArrival);
-    //console.log(childSnapshot.val().MinutesAway);
-
-    $("tbody").append(tr);
-    tr.append(tdTrain);
-    tr.append(tdDestination);
-    tr.append(tdFrequency);
-    tr.append(tdNextArrival);
-    tr.append(tdMinutesAway);
-
-    tdTrain.text(childSnapshot.val().TrainName);
-    tdDestination.text(childSnapshot.val().Destination);
-    tdFrequency.text(childSnapshot.val().Frequency);
-    tdNextArrival.text(moment(nextTrain).format("hh:mm"));
-    tdMinutesAway.text(tMinutesTillTrain);
-
-
-
-    //$("#train-name").text(childSnapshot.val().TrainName);
-    //$("#destination").text(childSnapshot.val().Destination);
-    //$("#frequency").text(childSnapshot.val().Frequency);
-    //$("#nextArrival").text(childSnapshot.val().TrainName);
-    // $("#minutesAway").text(childSnapshot.val().TrainName);
-
-
-})
+        tdTrain.text(childSnapshot.val().TrainName);
+        tdDestination.text(childSnapshot.val().Destination);
+        tdFrequency.text(childSnapshot.val().Frequency);
+        tdNextArrival.text(moment(nextTrain).format("HH:mm"));
+        tdMinutesAway.text(minutesAway);
+    })
 })
